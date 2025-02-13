a78a48442ead5b8f47b8e2bec6eb032fbb68ac8b
DavBaquero <david.baquero.19@gmail.com>
2025-02-13 11:25:32 +0100

    feat() admin create user

--- src/main/java/madstodolist/controller/AdminController.java
+++ src/main/java/madstodolist/controller/AdminController.java
@@ -1,16 +1,53 @@
 package madstodolist.controller;
 
+import madstodolist.dto.RegistroData;
+import madstodolist.dto.UsuarioData;
+import madstodolist.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.Collections;
 
 @Controller
 public class AdminController {
+
+    @Autowired
+    UsuarioService usuarioService;
 
     @GetMapping("/admin")
     public String mostrarTienda(HttpSession session) {
         session.getAttribute("userId");
         return "admin";
     }
+
+    @PostMapping("/admin/registro")
+    public ResponseEntity<String> registroSubmit(@RequestBody @Valid RegistroData registroData, BindingResult result) {
+        System.out.println("Solicitud recibida: " + registroData);  // <-- Log en backend
+
+        if (result.hasErrors()) {
+            return ResponseEntity.badRequest().body("Datos inválidos");
+        }
+
+        if (usuarioService.findByEmail(registroData.getEmail()) != null) {
+            return ResponseEntity.badRequest().body("El usuario " + registroData.getEmail() + " ya existe");
+        }
+
+        UsuarioData usuario = new UsuarioData();
+        usuario.setEmail(registroData.getEmail());
+        usuario.setPassword(registroData.getPassword());
+        usuario.setFechaNacimiento(registroData.getFechaNacimiento());
+        usuario.setNombre(registroData.getNombre());
+
+        usuarioService.registrar(usuario);
+        return ResponseEntity.ok("Usuario registrado exitosamente");
+    }
+
+
 }
